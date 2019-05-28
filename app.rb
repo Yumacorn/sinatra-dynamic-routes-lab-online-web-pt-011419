a5a69e49d42f80109e690ca6d51747eb815ee6f1
Ryan Mui <reachryanmui@gmail.com>
2019-05-28 23:44:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,21 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
-  # get '/hello' do
-  #   "Hello you!"
-  # end
-  #
-  # get '/reversename/:name' do
-  #   @name = params[:name]
-  #   "#{@name} reversed is #{@name.reverse}!"
-  # end
-  #
-  # get '/square/:number' do
-  #   @num = params[:number]
-  #   "#{@num.to_i ** 2}"
-  # end
-  #
+  Write your code here!
+  get '/hello' do
+    "Hello you!"
+  end
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name} reversed is #{@name.reverse}!"
+  end
+
+  get '/square/:number' do
+    @num = params[:number]
+    "#{@num.to_i ** 2}"
+  end
+
   # get '/say/:number/:phrase' do
   #   @num = params[:number]
   #   @phrase = params[:phrase]
